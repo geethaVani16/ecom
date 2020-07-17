5888bb5a4a65cf1df114d306f06ecc2c5131b8c0
geetha vani <geetha53813@gmail.com>
2020-07-17 18:49:57 +0530

    added with router facility all menuitem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage.component';
+import { Route, Switch } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatPage} />
+        <Route path='/topics' component={TopicsList}/>
+        <Route extact path='/topics/:topicId' component={TopicsDetails}/>
+      </Switch>
+
+      {/* <HomePage/> */}
     </div>
   );
 }
 
 export default App;
+
+
+const HatPage = () => {
+  return (
+    <h1>
+      hatPage
+    </h1>
+  )
+}
+
+
+const TopicsList = (props) => {
+  console.log(props,"props")
+  return (
+    <h1>
+      TopicsList
+    </h1>
+  )
+}
+
+
+const TopicsDetails = (props) => {
+  console.log(props,"props")
+  return (
+    <>
+    <p>{props.params.match.topicId}</p>
+    <h1>
+      TopicsDetails
+    </h1>
+    </>
+  )
+}
