8da4bd4f02f890b7ea8d98c0115fee6791ac2282
geetha vani <geetha53813@gmail.com>
2020-10-20 00:15:35 +0530

    added redux persist functionality

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './Redux/store';
-
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './Redux/store';
 
 
 
@@ -14,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
